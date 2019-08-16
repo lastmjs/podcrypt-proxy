abe6541da2382e4307d7f1468acbe5e4a9d26a9c
Jordan Last <jordan.michael.last@gmail.com>
2019-08-16 15:34:27 -0700

    test

--- cors-proxy.ts
+++ cors-proxy.ts
@@ -1,38 +1,39 @@
 import * as corsProxy from 'cors-anywhere';
 
 const server = corsProxy.createServer({
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: [
-        'cookie',
-        'cookie2',
-        // Strip Heroku-specific headers
-        'x-heroku-queue-wait-time',
-        'x-heroku-queue-depth',
-        'x-heroku-dynos-in-use',
-        'x-request-start',
-      ],
-      redirectSameOrigin: true,
-    originWhitelist: [],
+    // requireHeader: ['origin', 'x-requested-with'],
+    // removeHeaders: [
+    //     'cookie',
+    //     'cookie2',
+    //     // Strip Heroku-specific headers
+    //     'x-heroku-queue-wait-time',
+    //     'x-heroku-queue-depth',
+    //     'x-heroku-dynos-in-use',
+    //     'x-request-start',
+    //   ],
+    //   redirectSameOrigin: true,
+    // originWhitelist: [],
     setHeaders: {
+        'accept': '*/*',
         // I believe the documentation says that the header keys here should be lowercase strings
-        // 'user-agent': 'Node.js',
-        'origin': 'https://proxy.podcrypt.app',
+        'user-agent': 'Node.js',
+        // 'origin': 'https://proxy.podcrypt.app',
         // 'x-requested-with': 'XMLHttpRequest'
         // httpProxyOptions: {
         //     followRedirects: true
         // }
     },
-    httpProxyOptions: {
-        xfwd: false,
-        // followRedirects: false,
-        headers: {
-            'origin': 'https://proxy.podcrypt.app',
-        //     'x-requested-with': 'XMLHttpRequest'    
-        }
-        // changeOrigin: true,
-        // hostRewrite: true,
-        // autoRewrite: true
-    }
+    // httpProxyOptions: {
+    //     xfwd: false,
+    //     // followRedirects: false,
+    //     headers: {
+    //         'origin': 'https://proxy.podcrypt.app',
+    //     //     'x-requested-with': 'XMLHttpRequest'    
+    //     }
+    //     // changeOrigin: true,
+    //     // hostRewrite: true,
+    //     // autoRewrite: true
+    // }
 });
 
 const port = process.env.PORT || 4000;

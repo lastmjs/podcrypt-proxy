556a26a514b0964bbb05372c0a3c5ad3c2473781
Jordan Last <jordan.michael.last@gmail.com>
2019-08-16 16:38:47 -0700

    test

--- cors-proxy.ts
+++ cors-proxy.ts
@@ -1,58 +1,16 @@
 import * as corsProxy from 'cors-anywhere';
 
+// TODO only allow podcrypt.app
 const server = corsProxy.createServer({
-    // requireHeader: ['origin', 'x-requested-with'],
-    // removeHeaders: [
-    //     'cookie',
-    //     'cookie2',
-    //     // Strip Heroku-specific headers
-    //     'x-heroku-queue-wait-time',
-    //     'x-heroku-queue-depth',
-    //     'x-heroku-dynos-in-use',
-    //     'x-request-start',
-    //   ],
-    //   redirectSameOrigin: true,
-    // originWhitelist: [],
     removeHeaders: [
-        // 'x-forwarded-proto',
-        // 'cookie',
-        // 'authority',
-        // 'method',
-        // 'path',
-        // 'scheme',
-        // 'accept',
-        // 'accept-encoding',
-        // 'accept-language',
-        // 'cache-control',
-        // 'pragma',
-        // 'upgrade-insecure-requests',
-        // 'referer',
-        // 'origin',
-        // 'content-type',
         'connection'
     ],
     setHeaders: {
-        // 'host': 'chtbl.com',
-        // 'accept': '*/*',
         // I believe the documentation says that the header keys here should be lowercase strings
-        // 'user-agent': 'Node.js',
         'user-agent': 'Node.js',
-        // 'origin': 'https://proxy.podcrypt.app',
-        // 'x-requested-with': 'XMLHttpRequest'
-        // httpProxyOptions: {
-        //     followRedirects: true
-        // }
     },
     httpProxyOptions: {
-        // xfwd: false,
-    //     // followRedirects: false,
-    //     headers: {
-    //         'origin': 'https://proxy.podcrypt.app',
-    //     //     'x-requested-with': 'XMLHttpRequest'    
-    //     }
-    //     // changeOrigin: true,
-    //     // hostRewrite: true,
-    //     // autoRewrite: true
+        // xfwd: false // TODO we should consider this for the privacy of the clients
     }
 });
 

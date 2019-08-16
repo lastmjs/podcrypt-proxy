dd38d091350433b8913b93e1f26d3c878067c834
Jordan Last <jordan.michael.last@gmail.com>
2019-08-16 15:06:48 -0700

    test

--- cors-proxy.ts
+++ cors-proxy.ts
@@ -1,22 +1,34 @@
 import * as corsProxy from 'cors-anywhere';
 
 const server = corsProxy.createServer({
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: [
+        'cookie',
+        'cookie2',
+        // Strip Heroku-specific headers
+        'x-heroku-queue-wait-time',
+        'x-heroku-queue-depth',
+        'x-heroku-dynos-in-use',
+        'x-request-start',
+      ],
+      redirectSameOrigin: true,
     originWhitelist: [],
-    setHeaders: {
+    // setHeaders: {
         // I believe the documentation says that the header keys here should be lowercase strings
-        'user-agent': 'Node.js',
-        'origin': 'https://proxy.podcrypt.app',
-        'x-requested-with': 'XMLHttpRequest'
+        // 'user-agent': 'Node.js',
+        // 'origin': 'https://proxy.podcrypt.app',
+        // 'x-requested-with': 'XMLHttpRequest'
         // httpProxyOptions: {
         //     followRedirects: true
         // }
-    },
+    // },
     httpProxyOptions: {
-        followRedirects: false,
-        headers: {
-            'origin': 'https://proxy.podcrypt.app',
-            'x-requested-with': 'XMLHttpRequest'    
-        }
+        xfwd: false,
+        // followRedirects: false,
+        // headers: {
+        //     'origin': 'https://proxy.podcrypt.app',
+        //     'x-requested-with': 'XMLHttpRequest'    
+        // }
         // changeOrigin: true,
         // hostRewrite: true,
         // autoRewrite: true
